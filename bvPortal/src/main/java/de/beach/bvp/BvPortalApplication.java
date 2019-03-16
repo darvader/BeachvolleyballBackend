7b8fdea1ac940949b89aab5212bddf3cad45b855
Andreas Fiedler <fiedler.andi@gmail.com>
2019-03-16 20:35:02 +0100

    Added security.

--- bvPortal/src/main/java/de/beach/bvp/BvPortalApplication.java
+++ bvPortal/src/main/java/de/beach/bvp/BvPortalApplication.java
@@ -1,5 +1,7 @@
 package de.beach.bvp;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,6 +15,7 @@ public class BvPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BvPortalApplication.class, args);
+		Logger.getGlobal().info("Version 1.");
 	}
 	
 	@Bean
--- bvPortal/src/main/java/de/beach/bvp/security/CustomWebSecurityConfigurerAdapter.java
+++ bvPortal/src/main/java/de/beach/bvp/security/CustomWebSecurityConfigurerAdapter.java
@@ -1,4 +1,4 @@
-/*package de.beach.bvp.security;
+package de.beach.bvp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -33,16 +35,24 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
     protected void configure(HttpSecurity http) throws Exception {
         http
           .authorizeRequests()
-          .anyRequest().permitAll()
+          .anyRequest()
+          .permitAll()
           .and()
           .httpBasic()
-          .authenticationEntryPoint(authenticationEntryPoint);
+          .authenticationEntryPoint(authenticationEntryPoint)
+          .and().csrf().disable()
+          ;
 
         http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class); 
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    	super.configure(web);
     }
     
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}*/
+}
